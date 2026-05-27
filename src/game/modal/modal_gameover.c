316797c28c580c48e9bd16e5f6bca22d31ed23b1
andy <aksommerville@gmail.com>
2026-05-27 16:27:07 -0400

    new gameover music

--- src/game/modal/modal_gameover.c
+++ src/game/modal/modal_gameover.c
@@ -123,7 +123,7 @@ static void gameover_compose_report() {
 void gameover_begin() {
   memset(&gameover,0,sizeof(gameover));
   g.modal=MODAL_GAMEOVER;
-  egg_play_song(1,RID_song_cleaned_clock,0,0.400,0.0);//TODO song. Currently borrowing from Queen of Clocks
+  egg_play_song(1,RID_song_unstolen,0,0.500,0.0);
   gameover_compose_report();
   
   double *v=gameover.jumpclock;
